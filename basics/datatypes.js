e3866bfc80fdce609ad7da73350dbd7cb770a4fa
Rohan Sakhare <rohanpatilsakhare@gmail.com>
2025-02-04 08:24:57 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"newer version"; // treat all js code as newer version 
+ 
+
+// alert ( 3 + 3 ) we are using node.js not browser
+
+ 
+console.log(3+33);
+
+//number
+// string 
+//bigint 
+// boolean -> true and false
+//null
+
+// symbols => unique 
+
+// object 
+
+
+console.log(typeof undefined);  // undefined
+console.log(typeof null) // object 
+
+
+
+
---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 12312
+let name = "serctey"
+var city = "city"
+
+/* 
+prefer not to use var 
+because of iisue in block scope and functional scope
+*/
