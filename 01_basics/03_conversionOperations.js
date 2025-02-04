8f819cb1d7def9117f47c1a8f79afaf6d458b8e9
Rohan Sakhare <rohanpatilsakhare@gmail.com>
2025-02-04 09:05:08 +0000

    added new files and renamed new files

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,21 @@
+let score = "33a"
+// 33 -> 33 
+// 33a -> NaN 
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+
+console.log(valueInnumber); 
+
+
+let num = 33 
+
+let stringNumber = String(num)
+console.log( 'value of ' + stringNumber)
+console.log( typeof(stringNumber));
+
+
+
